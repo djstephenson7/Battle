88048b4d807a11cf163c3a65481041d4ada88d6e
djstephenson7 <dan_0502@hotmail.co.uk>
2018-09-21 16:57:21 +0100

    add players as args in game class

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -2,14 +2,27 @@ require './lib/game'
 
 describe Game do
 
-  subject(:daniel) { Player.new('Daniel') }
-  subject(:jon) { Player.new('Jon') }
-  subject(:game) { described_class.new }
-  
+
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :jon }
+  let(:player_2) { double :daniel }
+
+  describe '#player_1' do
+    it 'retrieves the first player' do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
+  describe '#player_2' do
+    it 'retrieves the second player' do
+      expect(game.player_2).to eq player_2
+    end
+  end
+
     context '#attack' do
     it 'reduces player2 HP by 10' do
-      allow(jon).to receive(:receive_damage)
-      game.attack(daniel)
+      allow(player_2).to receive(:receive_damage)
+      game.attack(player_2)
     end
   end
 end
